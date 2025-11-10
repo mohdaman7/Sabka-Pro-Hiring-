5384d4883ad8b6d5d084f76122bd2649e110208e
mohdaman7 <muhammedaman2.oaman@gmail.com>
2025-11-10 10:44:58 +0530

    crm analytics wrapper update

--- app/crm/analytics/page.jsx
+++ app/crm/analytics/page.jsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import CRMLayout from "@/views/crm/CRMLayout";
 import AnalyticsDashboard from "@/views/crm/analytics/AnalyticsDashboard";
 
 export default function AnalyticsPage() {
-  return (
-    <CRMLayout>
-      <AnalyticsDashboard />
-    </CRMLayout>
-  );
+  return <AnalyticsDashboard />;
 }
